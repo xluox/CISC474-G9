96c24b1793a99c94ecaa2798550f5540cac2dd42
Yifu Li <wdljwan@gmail.com>
2019-09-09 13:29:08 -0400

    finised course work

---
+++ index.js
@@ -0,0 +1,8 @@
+$(function(){     //this code runs after page is fully loaded    
+    $("#nameform").on('submit',function(e){e.preventDefault();    
+    var fname=$('#fname').val();      
+    var lname=$('#lname').val(); 
+    var newHTML='<h1>'+fname+ ' '+lname+'</h1>';  
+    $(".jumbotron").removeClass('d-none').html(newHTML);
+    return false;  //prevents reload on submit    
+ }); });
